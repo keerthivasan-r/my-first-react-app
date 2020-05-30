466cf297796ad565e5197786bad81eb601a16246
Keerthivasan Ramanathan <keerthivasan.dev@gmail.com>
2020-05-30 18:13:53 +0530

    Add alternate player signs X and O

--- src/index.js
+++ src/index.js
@@ -2,34 +2,36 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            value : null,
-        }
-    }
-
-    render() {
-      return (
-        <button className="square" onClick={()=>{this.setState({value : 'X'})}}>
-          {this.state.value}
-        </button>
-      );
-    }
+  function Square(props) {
+    return (
+      <button className="square" onClick={props.onClick}>
+        {props.value}
+      </button>
+    );
   }
-  
+   
+ 
   class Board extends React.Component {
 
     constructor(props){
         super(props);
        this.state = {
-           squares : Array(9).fill(null)
+           squares : Array(9).fill(null),
+           xIsNext : true
        }
     }
 
     renderSquare(i) {
-      return <Square value={this.state.squares[i]}/>;
+      return <Square value={this.state.squares[i]} onClick={()=>this.handleClick(i)}/>;
+    }
+
+    handleClick(i){
+      const squares = this.state.squares.slice();
+      squares[i] = this.state.xIsNext ? 'X' : 'O';
+      this.setState({
+        squares,
+        xIsNext : !this.state.xIsNext
+      })
     }
   
     render() {

6ddb4a3c23a3d18bfa3ffd8068c003ba0edd289e
Keerthivasan Ramanathan <keerthivasan.dev@gmail.com>
2020-05-30 18:17:33 +0530

    Add status text for player move

--- src/index.js
+++ src/index.js
@@ -35,7 +35,7 @@ import './index.css';
     }
   
     render() {
-      const status = 'Next player: X';
+      const status = 'Next player: '+ (this.state.xIsNext ? 'X' : 'O');
   
       return (
         <div>
